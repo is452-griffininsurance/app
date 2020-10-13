590de01cc23dcc63dcb05b54a57ea8d78a8e85fc
Goi Jia Jian <jiajiannn@gmail.com>
2020-10-14 00:41:19 +0800

    feat: use Web3.givenProvider to connect to wallet instead

--- src/components/Blockchain/Block.js
+++ src/components/Blockchain/Block.js
@@ -1,39 +1,70 @@
 import React, { useState, useEffect } from "react";
 import Web3 from "web3";
-import CreditLetterFactory from "../../blockchain/abi";
+import CreditLetterFactory from "../../blockchain/abis/CreditLetterFactory.json";
 
 function Block() {
   const [block, setBlock] = useState([]);
-  useEffect(() => {
-    const web3 = new Web3(
-      new Web3.providers.HttpProvider(
-        "http://127.0.0.1:7545" // "https://mainnet.infura.io/v3/962df9d540344f349b3ca4c976f68d0d"
-      )
-    );
+  const [averageGasPrice, setAverageGasPrice] = useState(0);
+  const [contract, setContract] = useState();
 
+  useEffect(() => {
+    // Web3 server-side provider
+    // const web3 = new Web3(
+    //   new Web3.providers.HttpProvider(
+    //     "http://127.0.0.1:7545" // "https://mainnet.infura.io/v3/962df9d540344f349b3ca4c976f68d0d"
+    //   )
+    // );
+
+    const web3 = new Web3(Web3.givenProvider);
+
+    // Get average gas price
+    web3.eth
+      .getGasPrice()
+      .then((price) => {
+        console.log(`Average gas price: ${price}`);
+        setAverageGasPrice(price);
+      })
+      .catch(console.error);
+    // Get latest block
     web3.eth.getBlock("latest").then(setBlock);
-    const PAAddress = "0xf25186b5081ff5ce73482ad761db0eb0d25abfbf";
-    const PA = web3.eth.Contract(new CreditLetterFactory()).at(PAAddress);
-    console.log(PA);
+    const creditLetter = new web3.eth.Contract(CreditLetterFactory.abi);
+    console.log(window.ethereum);
+    window.ethereum.enable().then((account) => {
+      const defaultAccount = account[0];
+      web3.eth.defaultAccount = defaultAccount;
+    });
   }, []);
+
   return (
     <div>
-      <section className="hero is-dark">
+      <section className="hero is-dark mb-4">
         <div className="hero-body">
           <div className="container">
             <h1 className="title">Blockchain</h1>
-            <h2 className="subtitle">Current Block</h2>
+            <h2 className="subtitle">Interact with wallet</h2>
           </div>
         </div>
       </section>
       <div className="container">
-        {/* <nav class="breadcrumb" aria-label="breadcrumbs">
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li class="is-active"><Link to="/" aria-current="page">Home</Link></li>
-          </ul>
-        </nav> */}
-        Current Ethereum block is {block.number}.
+        <div className="columns">
+          <div className="column is-6">
+            <div className="card">
+              <div className="card-content has-text-centered">
+                <div className="media">
+                  <div className="media-content">
+                    <p className="title is-4">MetaMask</p>
+                  </div>
+                </div>
+
+                <div className="content">
+                  Connect to the MetaMask Chrome extension
+                </div>
+              </div>
+            </div>
+            Current Ethereum block is {block.number}. Current average gas price
+            is {averageGasPrice}.
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/Stocks.js
+++ src/components/Stocks.js
@@ -1,78 +1,66 @@
 import React, { useState, useEffect } from "react";
-import unirest from "unirest";
 
 function Stocks() {
   const [news, setNews] = useState([]);
   useEffect(() => {
-    const req = unirest(
-      "GET",
-      "https://yahoo-finance15.p.rapidapi.com/api/yahoo/ne/news/AAPL"
-    );
-    req.headers({
-      "x-rapidapi-host": "yahoo-finance15.p.rapidapi.com",
-    });
-    req.end(function () {
-      // if (res.error) throw new Error(res.error);
-      setNews([
-        {
-          description:
-            "Dow Jones futures: The stock market rally could go…us but stay engaged. Tesla fell on S&P; 500 news.",
-          guid: "9803606d-a324-3864-83a8-2bd621e6ccbd",
-          link:
-            "https://finance.yahoo.com/m/9803606d-a324-3864-83a…1e6ccbd/dow-jones-futures%3A-after.html?.tsrc=rss",
-          pubDate: "Sun, 06 Sep 2020 16:45:01 +0000",
-          title:
-            "Dow Jones Futures: After Stock Market Rally Dive, What To Do Now; Tesla Gets S&P 500 Surprise",
-        },
-        {
-          description:
-            "* Benzinga has examined the prospects for many inv…t provide investment advice. All rights reserved.",
-          guid: "efd2e7ba-aee1-3805-991c-953f98a94fb2",
-          link:
-            "https://finance.yahoo.com/news/benzingas-bulls-bears-week-apple-131736560.html?.tsrc=rss",
-          pubDate: "Sun, 06 Sep 2020 13:17:36 +0000",
-          title:
-            "Benzinga's Bulls And Bears Of The Week: Apple, Costco, FedEx And More",
-        },
-        {
-          description:
-            "On CNBC's &quot;Options Action,&quot; Tony Zha…t provide investment advice. All rights reserved.",
-          guid: "69daf284-bf60-33e4-af72-47b4ada6f83e",
-          link:
-            "https://finance.yahoo.com/news/tony-zhangs-apple-options-trade-125004404.html?.tsrc=rss",
-          pubDate: "Sun, 06 Sep 2020 12:50:04 +0000",
-          title: "Tony Zhang's Apple Options Trade",
-        },
-        {
-          description:
-            "Marketing campaigns have started touting the natio…ather after a relentless rally off of March lows.",
-          guid: "4c669879-5beb-3aae-b169-718d8a3cfebd",
-          link:
-            "https://finance.yahoo.com/m/4c669879-5beb-3aae-b16…8a3cfebd/3-top-5g-stocks-to-buy-in.html?.tsrc=rss",
-          pubDate: "Sun, 06 Sep 2020 11:00:00 +0000",
-          title: "3 Top 5G Stocks to Buy in September",
-        },
-        {
-          description:
-            "Warren Buffett is about as different from the ster…ost popular Robinhood stocks that Buffett adores.",
-          guid: "1e6626b0-2e0c-3d2c-8eae-4b89fb2642cc",
-          link:
-            "https://finance.yahoo.com/m/1e6626b0-2e0c-3d2c-8ea…89fb2642cc/3-robinhood-stocks-that.html?.tsrc=rss",
-          pubDate: "Sun, 06 Sep 2020 10:44:00 +0000",
-          title: "3 Robinhood Stocks That Warren Buffett Absolutely Loves",
-        },
-        {
-          description:
-            "Few events have garnered more attention this year …39;s respective 4-for-1 and 5-for-1 stock splits.",
-          guid: "22a5b8b2-8ed1-3355-b618-a7bcb53dfb26",
-          link:
-            "https://finance.yahoo.com/m/22a5b8b2-8ed1-3355-b61…3dfb26/4-things-learned-from-apple.html?.tsrc=rss",
-          pubDate: "Sun, 06 Sep 2020 10:36:00 +0000",
-          title: "4 Things Learned From Apple and Tesla Stock Splits",
-        },
-      ]);
-      // setNews(res.body?.item);
-    });
+    setNews([
+      {
+        description:
+          "Dow Jones futures: The stock market rally could go…us but stay engaged. Tesla fell on S&P; 500 news.",
+        guid: "9803606d-a324-3864-83a8-2bd621e6ccbd",
+        link:
+          "https://finance.yahoo.com/m/9803606d-a324-3864-83a…1e6ccbd/dow-jones-futures%3A-after.html?.tsrc=rss",
+        pubDate: "Sun, 06 Sep 2020 16:45:01 +0000",
+        title:
+          "Dow Jones Futures: After Stock Market Rally Dive, What To Do Now; Tesla Gets S&P 500 Surprise",
+      },
+      {
+        description:
+          "* Benzinga has examined the prospects for many inv…t provide investment advice. All rights reserved.",
+        guid: "efd2e7ba-aee1-3805-991c-953f98a94fb2",
+        link:
+          "https://finance.yahoo.com/news/benzingas-bulls-bears-week-apple-131736560.html?.tsrc=rss",
+        pubDate: "Sun, 06 Sep 2020 13:17:36 +0000",
+        title:
+          "Benzinga's Bulls And Bears Of The Week: Apple, Costco, FedEx And More",
+      },
+      {
+        description:
+          "On CNBC's &quot;Options Action,&quot; Tony Zha…t provide investment advice. All rights reserved.",
+        guid: "69daf284-bf60-33e4-af72-47b4ada6f83e",
+        link:
+          "https://finance.yahoo.com/news/tony-zhangs-apple-options-trade-125004404.html?.tsrc=rss",
+        pubDate: "Sun, 06 Sep 2020 12:50:04 +0000",
+        title: "Tony Zhang's Apple Options Trade",
+      },
+      {
+        description:
+          "Marketing campaigns have started touting the natio…ather after a relentless rally off of March lows.",
+        guid: "4c669879-5beb-3aae-b169-718d8a3cfebd",
+        link:
+          "https://finance.yahoo.com/m/4c669879-5beb-3aae-b16…8a3cfebd/3-top-5g-stocks-to-buy-in.html?.tsrc=rss",
+        pubDate: "Sun, 06 Sep 2020 11:00:00 +0000",
+        title: "3 Top 5G Stocks to Buy in September",
+      },
+      {
+        description:
+          "Warren Buffett is about as different from the ster…ost popular Robinhood stocks that Buffett adores.",
+        guid: "1e6626b0-2e0c-3d2c-8eae-4b89fb2642cc",
+        link:
+          "https://finance.yahoo.com/m/1e6626b0-2e0c-3d2c-8ea…89fb2642cc/3-robinhood-stocks-that.html?.tsrc=rss",
+        pubDate: "Sun, 06 Sep 2020 10:44:00 +0000",
+        title: "3 Robinhood Stocks That Warren Buffett Absolutely Loves",
+      },
+      {
+        description:
+          "Few events have garnered more attention this year …39;s respective 4-for-1 and 5-for-1 stock splits.",
+        guid: "22a5b8b2-8ed1-3355-b618-a7bcb53dfb26",
+        link:
+          "https://finance.yahoo.com/m/22a5b8b2-8ed1-3355-b61…3dfb26/4-things-learned-from-apple.html?.tsrc=rss",
+        pubDate: "Sun, 06 Sep 2020 10:36:00 +0000",
+        title: "4 Things Learned From Apple and Tesla Stock Splits",
+      },
+    ]);
   }, []);
   return (
     <div>
