5a405bff86c3bb773d04afb4ab8fdae1f5d90a4c
Goi Jia Jian <jiajiannn@gmail.com>
2020-10-14 14:57:31 +0800

    feat: ropsten contract interaction (#3)

--- src/components/Blockchain/Block.js
+++ src/components/Blockchain/Block.js
@@ -1,22 +1,23 @@
 import React, { useState, useEffect } from "react";
 import Web3 from "web3";
-import CreditLetterFactory from "../../blockchain/abis/CreditLetterFactory.json";
+import CreditLetter from "../../blockchain/abis/CreditLetter.json";
+import OnboardingButton from "./OnboardingButton";
 
 function Block() {
   const [block, setBlock] = useState([]);
   const [averageGasPrice, setAverageGasPrice] = useState(0);
+  const [currentAddress, setCurrentAddress] = useState(0x00);
+  const [currentLetterOfCredit, setCurrentLetterOfCredit] = useState([]);
+
+  // Web3 client-side provider
   const web3 = new Web3(Web3.givenProvider);
-
-  // const [contract, setContract] = useState();
-
+  // Web3 server-side provider
+  // const web3 = new Web3(
+  //   new Web3.providers.HttpProvider(
+  //     "http://127.0.0.1:7545" // "https://mainnet.infura.io/v3/962df9d540344f349b3ca4c976f68d0d"
+  //   )
+  // );
   useEffect(() => {
-    // Web3 server-side provider
-    // const web3 = new Web3(
-    //   new Web3.providers.HttpProvider(
-    //     "http://127.0.0.1:7545" // "https://mainnet.infura.io/v3/962df9d540344f349b3ca4c976f68d0d"
-    //   )
-    // );
-
     // Get average gas price
     web3.eth
       .getGasPrice()
@@ -30,25 +31,68 @@ function Block() {
     window.ethereum.enable().then((account) => {
       const defaultAccount = account[0];
       web3.eth.defaultAccount = defaultAccount;
+      setCurrentAddress(defaultAccount);
     });
-
-    // Deploying L/C from the frontend (think for us, we do on backend instead)
-    const creditLetter = new web3.eth.Contract(CreditLetterFactory.abi);
-    creditLetter
-      .deploy({
-        data: `${CreditLetterFactory.bytecode}`,
-        arguments: [
-          web3.eth.defaultAccount,
-          web3.utils.toHex(1000), // need to toHex
-          web3.utils.toHex(1),
-        ],
-      })
-      .send({
-        from: "0x6D9007d50950F2a8ddd44795082AAA4fEBb5Cb73",
-        gasPrice: "1000",
-        gas: 2310334,
-      });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  function DeployContractButton() {
+    const deployLC = () => {
+      // Deploying contract should be done through our backend service
+      const creditLetter = new web3.eth.Contract(CreditLetter.abi);
+      creditLetter
+        .deploy({
+          data: `${CreditLetter.bytecode}`,
+          arguments: [
+            currentAddress,
+            currentAddress,
+            currentAddress,
+            web3.utils.toHex(1), // need to toHex
+            web3.utils.toHex(2),
+            web3.utils.toHex(1800),
+          ],
+        })
+        .send({
+          from: currentAddress,
+          gasPrice: averageGasPrice,
+          gas: 2310334,
+        });
+    };
+    return (
+      <input
+        className="button"
+        type="submit"
+        value="Deploy Letter of Credit"
+        onClick={deployLC}
+      />
+    );
+  }
+
+  function ContractInteractionButton() {
+    const interactWithLC = () => {
+      const creditLetter = new web3.eth.Contract(
+        CreditLetter.abi,
+        "0x177F046beb15F1a263638b8211ff6B0EaBD2b455"
+      );
+
+      creditLetter.methods
+        .getInfo()
+        .call({ from: currentAddress })
+        .then((response) => {
+          console.info(response);
+          setCurrentLetterOfCredit(response);
+        });
+    };
+
+    return (
+      <input
+        className="button"
+        type="submit"
+        value="Read-only Interaction"
+        onClick={interactWithLC}
+      />
+    );
+  }
 
   return (
     <div>
@@ -70,14 +114,50 @@ function Block() {
                     <p className="title is-4">MetaMask</p>
                   </div>
                 </div>
-
                 <div className="content">
                   Connect to the MetaMask Chrome extension
                 </div>
+                <div className="content">
+                  <OnboardingButton />
+                </div>
               </div>
             </div>
-            Current Ethereum block is {block.number}. Current average gas price
-            is {averageGasPrice}.{web3.eth.defaultAccount}
+          </div>
+        </div>
+        <div className="columns">
+          <div className="column is-6">
+            <div>
+              Current Ethereum block is {block.number}. Current average gas
+              price is {averageGasPrice}.{web3.eth.defaultAccount}
+              <DeployContractButton /> <ContractInteractionButton />
+            </div>
+          </div>
+        </div>
+        <div className="columns">
+          <div className="column is-6">
+            <div>Letter of Credit details.</div>
+            <div>
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th>
+                      <abbr>Key</abbr>
+                    </th>
+                    <th>Value</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {Object.keys(currentLetterOfCredit).map((key) => {
+                    return (
+                      <tr key={key}>
+                        <td>{key}</td>
+                        <td>{currentLetterOfCredit[key]}</td>
+                      </tr>
+                    )
+                  })}
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
       </div>
