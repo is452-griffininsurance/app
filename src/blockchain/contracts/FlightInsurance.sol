6996c4028eb3d7f79fa8db7ea88bd3bcc7e87766
Goi Jia Jian <jiajiannn@gmail.com>
2020-11-05 15:07:21 +0800

    feat: update contract

--- src/blockchain/contracts/FlightInsurance.sol
+++ src/blockchain/contracts/FlightInsurance.sol
@@ -17,15 +17,11 @@ contract FlightInsurance is ISmartInsurance {
     // It's also payable in case it expires
     // address payable[] public insurers;
     mapping(address => address payable) public insurers;
-    mapping(address => uint256) public balances; // Specific insurers total
-    uint256 public insurersTotal = 0; // Total amount ONLY for the insurers pool
-
-    // Minimum, maximum and total amount for the insurers pool
-    uint256 public minInsureAmount = 0;
-    uint256 public maxInsureAmount = 0;
+    mapping(address => uint256) public balances;
     
     // Our address as escrow 0xfeB87197aBd18dDaBD28B58b205936dfB4569B17
     address payable public escrow = payable(0xfeB87197aBd18dDaBD28B58b205936dfB4569B17);
+    address payable public commissions = payable(0xc008b0ce010280fa0e0a367df632ceba7d89e7ed7a5e3d2e05d7652b0094501e);
     
     string public flightCode;
     string public flightDate;
@@ -40,25 +36,19 @@ contract FlightInsurance is ISmartInsurance {
         _;
     }
     
-    constructor(string memory _flightCode, string memory _flightDate, uint256 _minInsureAmount, uint256 _maxInsureAmount) payable public {
+    constructor(string memory _flightCode, string memory _flightDate) payable public {
         require(msg.value > 0, "Your premium must be at least 1 wei.");
         insured = msg.sender;
         flightCode = _flightCode;
         flightDate = _flightDate;
-        minInsureAmount = _minInsureAmount;
-        maxInsureAmount = _maxInsureAmount;
         premium = premium.add(msg.value);
         status = Status.AWAITING_FUNDING;
     }
     
     function insure() payable public override {
-        require(msg.value > 0, "Your Insure Amount cannot be zero value.");
-        require(msg.value >= minInsureAmount, "Your insure amount must be more than this Insurance contract's Minimum Insure Amount.");
-        require(insurersTotal.add(msg.value) <= maxInsureAmount, "Your insure amount cannot exceed the Insurance contract's Insurers Pool Amount.");
-
+        require(msg.value > 0, "You must insure this insurance with at least 1 wei.");
         insurers[msg.sender] = msg.sender;
         balances[msg.sender] = balances[msg.sender].add(msg.value);
-        insurersTotal = insurersTotal.add(msg.value);
     }
 
     function getBalance() public view override returns (uint256) {
@@ -78,8 +68,7 @@ contract FlightInsurance is ISmartInsurance {
         // The payable to address cannot be the escrow
         require(escrow != to, "You cannot transfer money that doesn't belong to you!");
         // The payable to address must either be the insured or the insurers
-        require(insurers[to] == to || insured == to, "This insurance contract can only payout to registered insurers or the insured.");
-        
+        require(insurers[to] == to || insured == to || commissions == to , "This insurance contract can only payout to registered insurers, or the insured, or the commissions address.");
         if (insured == to) {
             status = Status.PAID_OUT;
         }
